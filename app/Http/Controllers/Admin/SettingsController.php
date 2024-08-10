e14a61762ffd18de40decfd8a3b09bc8b9f7128f
kareem22T <m.othman@clicksegypt.net>
2024-08-11 00:42:27 +0300

    edits

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Setting;
 use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\Storage;
 
 class SettingsController extends Controller
 {
