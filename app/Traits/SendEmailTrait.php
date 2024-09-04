8040a94f46f7e44b5666456ae50300c6d0a2632d
kareem22T <m.othman@clicksegypt.net>
2024-09-04 20:02:02 +0300

    edis

--- app/Traits/SendEmailTrait.php
+++ app/Traits/SendEmailTrait.php
@@ -17,7 +17,7 @@ trait SendEmailTrait
 
         try {
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+            $mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL; //Enable verbose debug output
             $mail->isSMTP(); //Send using SMTP
             $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
             $mail->SMTPAuth = true; //Enable SMTP authentication
