4931185d7d7947ad46b4e7ac2b5cb4c46c5c108c
kareem22T <m.othman@clicksegypt.net>
2024-09-07 11:03:34 +0300

    edits

--- app/Repositories/DoctorRepository.php
+++ app/Repositories/DoctorRepository.php
@@ -65,7 +65,7 @@ class DoctorRepository implements DoctorRepositoryInterface
                 $data['photo'] = $this->storePhoto($data['photo']);
             }
 
-            if (isset($data['password']) && !isEmpty($data['password'])) {
+            if (isset($data['password']) && $data['password']) {
                 $data['password'] = Hash::make($data['password']);
             }
 
