9fae71e299cac53eb46422a9c85f64b1a16987c1
kareem22T <m.othman@clicksegypt.net>
2024-09-14 02:35:12 +0300

    edits

--- app/Http/Controllers/User/AppointmentController.php
+++ app/Http/Controllers/User/AppointmentController.php
@@ -34,7 +34,7 @@ class AppointmentController extends Controller
     }
 
     public function getAll() {
-        $appointments = Appointment::latest()->with(['doctor', 'user'])->paginate('10');
+        $appointments = Appointment::latest()->with(['doctor', 'user'])->paginate('2');
         return $appointments;
     }
 
